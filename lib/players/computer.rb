49f3340cf4c50b775c4da8330f2aa0ab93e21c37
Derek Haller <dhaller.personal@gmail.com>
2018-02-11 05:03:15 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,22 +4,13 @@ module Players
       i = 4
       ai = 0
       while board.taken?(i+1)
-<<<<<<< HEAD
         i += 2
-=======
-        i + 2
->>>>>>> d99e0e1836e7695bd36857555e76d0a7193fb63f
         if i >= 10
           i = 0
           ai += 1
         end
-<<<<<<< HEAD
         if i == 0 && ai == 2
           i += 1
-=======
-        if i == 2 && ai == 1
-          i+1
->>>>>>> d99e0e1836e7695bd36857555e76d0a7193fb63f
         end
       end
       i += 1
